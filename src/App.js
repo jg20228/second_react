56f0362a97916a2636eee5dc01c5ce063bb51bf5
jg20228 <jg20228@nate.com>
2020-07-21 17:52:04 +0900

    ref

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  state = {
-    password: "",
-    clicked: false,
-    validate: false,
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      password: e.target.value,
-    });
+  changeFocus = () => {
+    this.input.focus();
+    this.mydiv.style.backgroundColor = "yellow";
   };
 
   render() {
     return (
       <div>
         <input
-          type="password"
-          value={this.state.password}
-          onChange={this.handleChange}
+          ref={(ref) => {
+            this.input = ref;
+          }}
+          type="text"
+          placeholder="Username"
         />
-        <button onClick="validate()">검증하기</button>
+        <input type="text" placeholder="password" />
+
+        <div
+          ref={(ref) => {
+            this.mydiv = ref;
+          }}
+        >
+          fasd
+        </div>
+
+        <button onClick={this.changeFocus}>포커스 이동</button>
       </div>
     );
   }
