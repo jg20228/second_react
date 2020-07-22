816b7fd8b4c249341631525f4b2d5666d7c9d8fe
jg20228 <jg20228@nate.com>
2020-07-22 18:17:39 +0900

    버튼으로 추가삭제

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   //배열을 하나(데이터) 만듬
   //이 데이터를 바인딩해서 뿌릴것이다.
   state = {
-    post: [
+    posts: [
       {
         id: 1,
         title: "제목1",
@@ -28,10 +28,29 @@ class App extends Component {
       justify-content: center;
     `;
 
+    //render 안에서는 const로 해야함
+    //화면그려질때마다 계속 만들 필요없어서
+    //오브젝트 넣을것이라 중괄호
+    const add = () => {
+      this.setState({
+        posts: this.state.posts.concat({ id: 4, title: "제목4" }),
+      });
+    };
+    //중괄호 적었으니 return
+    const del = () => {
+      this.setState({
+        posts: this.state.posts.filter((post) => {
+          return post.id !== 2;
+        }),
+      });
+    };
+
     return (
-      //데이터를 뿌림
+      //데이터를 뿌림, JSX 문법 onClick 조심
       <ContainerBox>
-        {this.state.post.map((post) => {
+        <button onClick={del}>삭제</button>
+        <button onClick={add}>추가</button>
+        {this.state.posts.map((post) => {
           return <Post id={post.id} title={post.title} />;
         })}
       </ContainerBox>
