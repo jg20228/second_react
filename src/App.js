7c9fda4071a74f2ed9e30ba46ba290820f5d5b76
jg20228 <jg20228@nate.com>
2020-07-23 17:02:11 +0900

    중간요소추가

--- src/App.js
+++ src/App.js
@@ -2,32 +2,24 @@ import React, { Component } from "react";
 import Post from "./Post";
 import styled from "styled-components";
 
+//스타일 컴포넌트는 무조건 밖으로 뺀다
+const ContainerBox = styled.div`
+  display: grid;
+  justify-content: center;
+`;
+
 class App extends Component {
   //배열을 하나(데이터) 만듬
   //이 데이터를 바인딩해서 뿌릴것이다.
   state = {
     posts: [
-      {
-        id: 1,
-        title: "제목1",
-      },
-      {
-        id: 2,
-        title: "제목2",
-      },
-      {
-        id: 3,
-        title: "제목3",
-      },
+      { id: 1, title: "제목1" },
+      { id: 2, title: "제목2" },
+      { id: 3, title: "제목3" },
     ],
   };
 
   render() {
-    const ContainerBox = styled.div`
-      display: grid;
-      justify-content: center;
-    `;
-
     //render 안에서는 const로 해야함
     //화면그려질때마다 계속 만들 필요없어서
     //오브젝트 넣을것이라 중괄호
