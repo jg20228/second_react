891bd4298eb26fa37571414a7b4587a2880c8c30
jg20228 <jg20228@nate.com>
2020-07-27 17:01:16 +0900

    리액트 부트스트랩 적용

--- src/App.js
+++ src/App.js
@@ -1,66 +1,12 @@
 import React, { Component } from "react";
-import Post from "./Post";
-import styled from "styled-components";
-
-//스타일 컴포넌트는 무조건 밖으로 뺀다
-const ContainerBox = styled.div`
-  display: grid;
-  justify-content: center;
-`;
+import Nav from "./Nav";
 
 class App extends Component {
-  //배열을 하나(데이터) 만듬
-  //이 데이터를 바인딩해서 뿌릴것이다.
-  state = {
-    posts: [
-      { id: 1, title: "제목1" },
-      { id: 2, title: "제목2" },
-      { id: 3, title: "제목3" },
-    ],
-  };
-
   render() {
-    //render 안에서는 const로 해야함
-    //화면그려질때마다 계속 만들 필요없어서
-    //오브젝트 넣을것이라 중괄호
-    const add = () => {
-      this.setState({
-        posts: this.state.posts.concat({ id: 4, title: "제목4" }),
-      });
-    };
-    //중괄호 적었으니 return
-    const del = () => {
-      this.setState({
-        posts: this.state.posts.filter((post) => {
-          return post.id !== 2;
-        }),
-      });
-    };
-
-    //map을 사용해서 수정만들기 2번데이터 수정
-    const update = () => {
-      const data = {
-        id: 2,
-        title: "제목200",
-      };
-
-      this.setState({
-        posts: this.state.posts.map((post) =>
-          post.id === 2 ? { ...post, ...data } : post
-        ),
-      });
-    };
-
     return (
-      //데이터를 뿌림, JSX 문법 onClick 조심
-      <ContainerBox>
-        <button onClick={del}>삭제</button>
-        <button onClick={add}>추가</button>
-        <button onClick={update}>수정</button>
-        {this.state.posts.map((post) => {
-          return <Post id={post.id} title={post.title} />;
-        })}
-      </ContainerBox>
+      <div>
+        <Nav />
+      </div>
     );
   }
 }
